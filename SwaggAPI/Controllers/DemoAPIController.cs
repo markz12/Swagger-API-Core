a3f9e12b5f5c438e79c2aa4c082b0004584d817c
Mark Joseph V. Abelardo <markjoseph.abelardo18@gmail.com>
2021-02-25 13:19:26 +0800

    Add project files.

---
+++ SwaggAPI/Controllers/DemoAPIController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult DemoSwagger()
+        {
+            return Ok();
+        }
+    }
+}
