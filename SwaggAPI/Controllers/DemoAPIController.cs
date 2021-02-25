861182b574c27844f4a07b2caac601d0c8024c5a
Mark Joseph V. Abelardo <markjoseph.abelardo18@gmail.com>
2021-02-25 14:27:20 +0800

    Final Changes for Test Swagger

--- SwaggAPI/Controllers/DemoAPIController.cs
+++ SwaggAPI/Controllers/DemoAPIController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SwaggAPI.Model;
 
 namespace SwaggAPI.Controllers
 {
@@ -11,10 +12,61 @@ namespace SwaggAPI.Controllers
     [ApiController]
     public class DemoAPIController : ControllerBase
     {
-        [HttpGet]
+        [HttpGet(nameof(DemoSwagger))]
         public IActionResult DemoSwagger()
         {
-            return Ok();
+            DemoTest demo = new DemoTest
+            {
+                testName = "Swagger here",
+                testDescription = "This is for swagger api testing",
+                testDate = DateTime.Now
+            };
+            return Ok(demo);
+        }
+
+        [HttpPost(nameof(DemoPostSwagger))]
+        public IActionResult DemoPostSwagger(DemoTest demo)
+        {
+            //This will be used for testing.
+            return Ok(demo);
+        }
+
+        [HttpPut(nameof(DemoPutSwagger))]
+        public IActionResult DemoPutSwagger(string testName)
+        {
+            DemoTest demo = new DemoTest
+            {
+                testName = "MarkVanz Swagger",
+                testDescription = "This is for swagger api testing",
+                testDate = DateTime.Now
+            };
+            if (demo.testName.Equals(testName))
+            {
+                return Ok("Successfully Updated Swagger!");
+            }
+            else
+            {
+                return NotFound("Not found Swagger!");
+            }
+        }
+
+        [HttpDelete(nameof(DemoDeleteSwagger))]
+        public IActionResult DemoDeleteSwagger(string testName)
+        {
+            DemoTest demo = new DemoTest
+            {
+                testName = "MarkVanz Swagger",
+                testDescription = "This is for swagger api testing",
+                testDate = DateTime.Now
+            };
+            if (demo.testName.Equals(testName))
+            {
+                return Ok("Successfully Deleted Swagger!");
+            }
+            else
+            {
+                return NotFound("Not found Swagger!");
+            }
         }
     }
 }
---
+++ SwaggAPI/Model/DemoTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggAPI.Model
+{
+    public class DemoTest
+    {
+        public string testName { get; set; }
+        public string testDescription { get; set; }
+        public DateTime testDate { get; set; }
+    }
+}
